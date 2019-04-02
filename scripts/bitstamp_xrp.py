2be8d99c82be211bd6cbc34534491c1217431c6d
YalDan <yaldan91@gmail.comq>
2019-04-02 13:28:19 +0000

    minor edits

---
+++ scripts/bitstamp_xrp.py
@@ -0,0 +1,29 @@
+import json
+from pymongo import MongoClient
+import websocket
+import datetime
+import re
+
+mongo_client = MongoClient('mongodb://localhost:27017/')
+db = mongo_client['cryptocurrency']
+bitstamp_coll = db['bitstamp']
+
+def on_message(mes):
+    message = json.loads(mes)
+    if message['event'] in 'trade':
+        result = {'_id': message['data']['id'], 'p': message['data']['price'], 'q': message['data']['amount'], 'sell_id': message['data']['sell_order_id'],
+              'buy_id': message['data']['buy_order_id'], 'side': message['data']['type'], 't': message['data']['microtimestamp'], 'c': re.sub('live_trades_', '', message['channel'])}
+        print(datetime.datetime.now())
+        print(result)
+        res = bitstamp_coll.insert_one(message)
+
+
+ws = websocket.WebSocketApp('wss://ws.bitstamp.net')
+
+ws.on_open = lambda self: self.send('{ "event": "bts:subscribe", '
+                                    '"data": {'
+                                    '"channel": "live_trades_xrpusd"}}')
+
+ws.on_message = lambda self, evt: on_message(evt)
+
+ws.run_forever(ping_interval=10, ping_timeout=5)
---
+++ scripts/hitbtc_xlm.py
@@ -0,0 +1,31 @@
+import json
+from pymongo import MongoClient
+from websocket import WebSocketApp
+import datetime
+
+mongo_client = MongoClient('mongodb://localhost:27017/')
+db = mongo_client['cryptocurrency']
+hitbtc_coll = db['hitbtc']
+
+def on_message(mes):
+    #print(mes)
+    message = json.loads(mes)
+    result = {'_id': message['params']['data'][0]['id'], 'p': message['params']['data'][0]['price'], 'q': message['params']['data'][0]['quantity'], 'side': message['params']['data'][0]['side'], 't': message['params']['data'][0]['timestamp'], 'c': message['params']['symbol']}
+    print(datetime.datetime.now())
+    print(result)
+    res = hitbtc_coll.insert_one(result)
+
+ws = WebSocketApp('wss://api.hitbtc.com/api/2/ws')
+
+ws.on_open = lambda self: self.send(json.dumps({
+    "method": "subscribeTrades",
+    #"product_ids": ["BTC-USD", "ETH-USD", "LTC-USD"],
+    "params":
+        {
+            "symbol": "XLMUSD"
+        }
+    }))
+
+ws.on_message = lambda self, evt: on_message(evt)
+
+ws.run_forever(ping_interval=10, ping_timeout=5)
