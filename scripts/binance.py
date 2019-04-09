234890fee1c2ad485c4ee96ed0dccc5571c89f25
YalDan <yaldan91@gmail.comq>
2019-04-09 15:28:25 +0000

    connect to mongodb

--- scripts/binance.py
+++ scripts/binance.py
@@ -15,7 +15,7 @@ def on_message(mes):
     res = binance_coll.insert_one(message)
     #print("BTC-USD:" + " " + message)
 
-ws = WebSocketApp('wss://stream.binance.com:9443/ws/bchusdt@trade/btcusdt@trade/eosusdt@trade/etcusdt@trade/ethusdt@trade/iotausdt@trade/xlmusdt@trade/xmrusdt@trade/xrpusdt@trade/zrxusdt@trade')
+ws = WebSocketApp('wss://stream.binance.com:9443/ws/bchusdt@trade/ltcusdt@trade/btcusdt@trade/eosusdt@trade/etcusdt@trade/ethusdt@trade/iotausdt@trade/xlmusdt@trade/xmrusdt@trade/xrpusdt@trade/zrxusdt@trade')
 
 ws.on_message = lambda self, evt: on_message(evt)
 
